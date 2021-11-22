85194224a15686611c606241c645cc606d66dec2
YvonneFang <45304065+YvonneFang@users.noreply.github.com>
2021-11-22 12:28:59 +0800

    Update mirror_sketch.js

--- mirror_sketch.js
+++ mirror_sketch.js
@@ -1,25 +1,27 @@
-let video;
+let img;
+
+function preload(){
+ img = loadImage("purple.png");  
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  video = createCapture(VIDEO);
-  video.size(width,height);
-  video.hide();
+
 }
 
 function draw() {
-  // background(86, 94, 168,10);
   background(100,100,100,10);
   let gridSize = 15;
-  video.loadPixels();
+  img.loadPixels();
   noStroke();
-  for (let x=0;x<video.width;x+=gridSize){
-    for (let y=0;y<video.height;y+=gridSize){
-      let index = (y*video.width+x)*4;
-      let r = video.pixels[index];
-      let d = map(r, 0, 255, 0, random(100));
+  for (let x=0;x<img.width;x+=gridSize){
+    for (let y=0;y<img.height;y+=gridSize){
+      let index = (y*img.width+x)*4;
+      let r = img.pixels[index];
+      let d = map(r, 0, 255, random(90), random(10));
       
-      fill(255,random(20));
-      ellipse(x,y,d);
+      fill(255,random(80));
+      ellipse(x*5,y*2.5,d);
     }
   }
   }
